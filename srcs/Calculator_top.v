174ae755c1459fbe85ae87fec1f5d9de4bfeea2d
DavidVR1804 <164957223+DavidVR1804@users.noreply.github.com>
2025-12-08 23:42:37 -0500

    Update Calculator_top.v

--- srcs/Calculator_top.v
+++ srcs/Calculator_top.v
@@ -14,146 +14,157 @@ module Calculator_top(
     output wire [3:0] an, 
     output wire [7:0] seg
 );
-
-    reg [2:0] op = 3'b0; 
-    // Debounced buttons
-    wire up_d; wire down_d; wire enter_d; wire back_d; wire rst_d;  
-    wire [7:0] calculator_result; // Output from calculator module
-    reg [31:0] segments; // Value displayed to the seven segment display
-    reg convert_value_enable; //Enable signal 
-
-    //States for the control unit
-    parameter IDLE = 0; //all 0s after reset
-    parameter VALUE_SET = 1;
-    parameter CONV = 2; //wait for value to bcd
-    parameter RES = 3; //display result
-    parameter OP = 4; //operation menu
-
-
-    reg [2:0] cs; //current state
-    reg [2:0] ns; //next state
-    //Control unit 
-    always @(*) begin
-        if(rst == 1) begin
-            op <= 0; 
-        end
-        case(cs)
-            IDLE: begin
-                segments <= 0; 
-                if(enter_d) begin
-                    ns <= VALUE_SET;
-                end
-                else if (up_d | down_d) begin
-                    ns <= OP;
-                end
-            end
-            VALUE_SET: begin
-                if(op < 4) begin // logic operation, display binary
-                    value_to_convert <= (1000 * calculator_result[3]) + (100 * calculator_result[2]) + (10 * calculator_result[1]) + (1* calculator_result[0]);
-                end else if(op == 5) begin //Subtraction, check if positive 
-                    if(calculator_result[7]) begin //Sign bit is negative
-                        value_to_convert <= {4{1'b0}, (~calculator_result) + 1}; // Make positive 
-                    end
-                    else value_to_convert <= {{4{1'b0}}, calculator_result}; 
-                end else begin
-                    value_to_convert <= {{4{1'b0}}, calculator_result}; 
-                end
-                ns <= CONV;
-            end
-            CONV: begin
-                segments <= segValues_op; 
-                convert_value_enable <= 1; 
-                if(bcd_val_rdy) begin
-                    ns <= RES;
-                end
-                else ns <= CONV; 
-            end
-            RES: begin
-                segments <= segValues_res; 
-                if (back_d) begin
-                    ns <= IDLE;
-                end
-                else if (enter_d) begin
-                    ns <= VALUE_SET;
-                end
-            end
-            OP: begin
-                segments <= segValues_op; 
-                if (enter_d) begin
-                    ns <= VALUE_SET;
-                end
-                else if(back_d) begin
-                    ns <= RES;
-                end
-                else if(up_d) begin
-                    op <= op + 1;
-                end
-                else if(down_d) begin
-                    op <= op - 1;
-                end
-            end
-
-        endcase 
-
-    end
-
+    
+    
+    
+    reg [2:0] cs;
+    reg [2:0] ns;
+    
+    parameter IDLE = 0;
+    parameter OP = 1;
+    parameter CONV = 2;
+    parameter RES = 3;
+    
+    
+    wire [7:0] calc_result;
+    reg [2:0] op;
+    
+    Calculator CALC(.a(a),.b(b),.op(op), .out(calc_result));
+    
+   
+    wire [15:0] BCD_result;
+    wire BCD_rdy;
+    val_bcd VAL(.clk(clk),.reset(rst), .count({{4{1'b0}}, calc_result}), .rdy(BCD_rdy), .BCD(BCD_result));
+    
+    
+    wire [31:0] BCD_seg;
+    reg [31:0] BCD_seg_buffer;
+    bcd_seg A(.BCD(BCD_result[15:12]),.s(BCD_seg[31:24]));
+    bcd_seg B(.BCD(BCD_result[11:8]),.s(BCD_seg[23:16]));
+    bcd_seg C(.BCD(BCD_result[7:4]),.s(BCD_seg[15:8]));
+    bcd_seg D(.BCD(BCD_result[3:0]),.s(BCD_seg[7:0]));
+    
+    
+    wire [31:0] op_seg;
+    op_seg OPS(.OP(op), .s(op_seg)); 
+    
+    parameter ZEROS = 32'b0000_0011_0000_0011_0000_0011_0000_0011;
+    
+    reg [31:0] display_value;
+    Multi_Driver DRIVER(.clk(clk), .segValues(display_value), .an(an), .seg(seg));
+    
+    wire up_d;
+    wire down_d;
+    wire enter_d;
+    wire back_d;
+    wire rst_d;
+    Debounce UDB(.btn(up),.clk(clk),.led(up_d));
+    Debounce DDB(.btn(down),.clk(clk),.led(down_d));
+    Debounce EDB(.btn(enter),.clk(clk),.led(enter_d));
+    Debounce BDB(.btn(back),.clk(clk),.led(back_d));
+    Debounce RDB(.btn(rst),.clk(clk),.led(rst_d));
+    
+    
+    reg UP;  
+    reg UP_p; 
     always @(posedge clk) begin
+        if(rst_d == 1) begin
+            UP <= 0;
+            UP_p <= 0;
+        end
+        if((UP_p != up_d) && (up_d)) begin
+            UP <= 1; 
+        end 
+        else UP <= 0; 
+        UP_p <= up_d;
+    end
+    reg DOWN; 
+    reg DOWN_p;
+    always @(posedge clk) begin
+        if(rst_d == 1) begin
+            DOWN <= 0;
+            DOWN_p <= 0;
+        end
+        if((DOWN_p != down_d) && (down_d)) begin
+            DOWN <= 1; 
+        end 
+        else DOWN <= 0; 
+        DOWN_p <= down_d;
+    end
+    reg BACK; 
+    reg BACK_p; 
+    always @(posedge clk) begin
+        if(rst_d == 1) begin
+            BACK <= 0;
+            BACK_p <= 0;
+        end
+        if((BACK_p != back_d) && (back_d)) begin
+            BACK <= 1; 
+        end 
+        else BACK <= 0; 
+        BACK_p <= back_d;
+    end
+    
+    
+    always@(posedge clk) begin
         if(rst_d) begin
-            cs <= IDLE; 
+            ns <= IDLE;
+            display_value <= ZEROS;
         end
         else begin
-            cs <= ns; 
+            case (cs)
+                IDLE: begin
+                    display_value <= ZEROS;
+                    op <= 0;
+                    if(UP | DOWN) begin
+                        ns <= OP;
+                        op <= 0;
+                    end
+                    else if(enter_d)
+                        ns <= CONV;
+                end
+                OP: begin
+                    display_value <= op_seg;
+                    
+                    if(up_d)
+                        op = op + 1;
+                    else if (down_d)
+                        op = op - 1;
+                    
+                    if(enter_d)
+                        ns <= CONV;
+                    else if(BACK)
+                        ns <= RES;
+                end
+                CONV: begin
+                    display_value <= op_seg;
+                    if(BCD_rdy)
+                        BCD_seg_buffer <= BCD_seg;
+                        ns <= RES;
+                end
+                RES: begin
+                    display_value <= BCD_seg_buffer;
+                    if(UP | DOWN) begin
+                        ns <= OP;
+                        op <= 0;
+                    end
+                    else if(enter_d)
+                        ns <= CONV;
+                    //else if(BACK)
+                        //ns <= IDLE;
+                end
+                default: begin
+                    ns <= IDLE;
+                    display_value <= ZEROS;
+                    op <= 0;
+                end
+        
+        
+        
+            endcase
         end
     end
-
-    // Handle op code scrolling 
-    // always @ (posedge up_d or posedge down_d) begin
-    //     if(up_d) begin
-    //         op <= op + 1; 
-    //     end
-    //     else if(down_d) begin
-    //         op <= op - 1; 
-    //     end
-    // end
-
-    //Debounce buttons 
-    Debounce dd(.btn(down), .clk(clk), .led(down_d));
-    Debounce ud(.btn(up), .clk(clk), .led(up_d));
-    Debounce ed(.btn(enter), .clk(clk), .led(enter_d));
-    Debounce bd(.btn(back), .clk(clk), .led(back_d));
-    Debounce rd(.btn(rst), .clk(clk), .led(rst_d)); 
-
-    // Instantiate Calculator 
-    Calculator calc(.a(a), .b(b), .op(op), .out(calculator_result));
-
-    //Convert stable calculator result into seven segment display values 
-    wire [31:0] segValues_res; 
-    wire [15:0] calc_result_bcd; 
-    reg [11:0] value_to_convert; 
-    wire bcd_val_rdy; 
-
-    val_bcd vbcd(
-        .clk(clk),  
-        .reset(rst_d), 
-        .en(convert_value_enable), // Enable 
-        .count(value_to_convert), 
-        .BCD(calc_result_bcd),
-        .rdy(bcd_val_rdy)
-    );
-
-
-    //Convert all values to seven segment outputs
-    genvar i;
-    generate 
-        for(i = 0; i < 4; i = i + 1) begin
-            bcd_seg x(.sw(calc_result_bcd[3 + 4 * i:4 * i]), segValues_res[8 * i + 1 : 8 * i]); 
-        end
-    endgenerate
-
-    wire [31:0] segValues_op; 
-    op_seg y(.OP(op), .s(segValues_op));
-
-    //Drive multi segment displays
-    Multi_Driver md(.segValues(segments), .clk(clk), .an(an), .seg(seg));
-
-endmodule
+    
+    always @(negedge clk) begin
+            cs <= ns;
+    end
